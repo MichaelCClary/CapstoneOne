f014a41b38240144068e0e4d521217f87c44378c
Michael <michaelcclary@gmail.com>
2021-04-20 10:49:05 -0500

    heroku bugs

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = (
         os.environ.get('DATABASE_URL', 'postgresql:///boardgames_db'))
     DEVELOPMENT = True
-    SECRET_KEY = os.environ.get(SECRET_KEY, "it's a secret")
+    SECRET_KEY = os.environ.get('SECRET_KEY', "it's a secret")
     DEBUG_TB_INTERCEPT_REDIRECTS = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = False
